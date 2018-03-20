279cd84961af9e418a0c30eb13b08838ad99ddb6
Thomas Miller <thomasmiller@Thomass-MBP.gateway>
2018-03-21 00:49:20 +1000

    finish vid 42

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,6 +1,24 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const Web3 = requie('web3');
+const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+beforeEach(() => {
+    // Get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchAccounts => {
+            console.log(fetchAccounts);
+        });
 
+
+
+    // Use one of those accounts to deploy
+    // the contract
+})
+
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+
+    });
+});
